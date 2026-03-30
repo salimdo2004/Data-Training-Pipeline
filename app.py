b2ed60219258faaa905282d071948a391ace2e62
salim douazi <148771981+salimdo2004@users.noreply.github.com>
2026-03-30 13:28:08 +0100

    Update NLTK download paths and clean imports
    Refactor NLTK downloads to use a temporary directory and remove redundant imports.

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ for intent in Datas['intents']:
 print(output_intentions)
 print(len(output_intentions))
 
-import nltk
+
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 nltk.download('stopwords')
@@ -74,12 +74,15 @@ stop_words = set(stopwords.words('english'))
 nltk.download('punkt_tab')
 
 import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import string
+import os
 
-nltk.download('punkt')
-nltk.download('stopwords')
+nltk_data_path = "/tmp/nltk_data"
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
+nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('stopwords', download_dir=nltk_data_path)
+nltk.download('wordnet', download_dir=nltk_data_path)
 
 stop_words = set(stopwords.words('french'))
 
